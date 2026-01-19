58ceecd82b48f043aeff7f048eb9b5d4629ec8f7
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2026-01-19 16:28:29 +0100

    first commit

---
+++ Formula/spt.rb
@@ -0,0 +1,38 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://docs.brew.sh/rubydoc/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Spt < Formula
+  desc "A Bash script for Simple Debian Tool. "
+  homepage "https://spt.codewithshin.com"
+  url "https://github.com/shinokada/spt/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "4e20bebc60bf34305f995afb02040ed2f43baa5e7b5a3dace2cace626e817b05"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  # Additional dependency
+  # resource "" do
+  #   url ""
+  #   sha256 ""
+  # end
+
+  def install
+    # Remove unrecognized options if they cause configure to fail
+    # https://docs.brew.sh/rubydoc/Formula.html#std_configure_args-instance_method
+    system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test spt`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system bin/"program", "do", "something"`.
+    system "false"
+  end
+end
